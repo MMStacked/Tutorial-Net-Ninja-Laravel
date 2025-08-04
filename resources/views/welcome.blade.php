165703449ccc32ce3f70838c9e61a88cdddb5725
Awais I <mmstacked@gmail.com>
2025-08-04 19:16:00 +0100

    Completed Lesson 7 - added css/tailwind to create a better ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tutorial Ninja Network</title>
+
+    @vite('resources/css/app.css')
+
 </head>
-<body>
+<body class="text-center px-8 py-12">
     <h1>Welcome to the Tutorial Ninja Network</h1>
     <p>Click the button below to view the list of ninjas.</p>
-    <a href="/ninjas" class="btn">Find Ninjas!</a>
+    <a href="/ninjas" class="btn mt-4 inline-block">Find Ninjas!</a>
 </body>
 </html>
