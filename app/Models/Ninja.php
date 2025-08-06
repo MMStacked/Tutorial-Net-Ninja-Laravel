cdf281cc9b03067245cce58c119770440d6eee74
Awais I <mmstacked@gmail.com>
2025-08-06 22:36:48 +0100

    Completed: Laravel Tutorial for Beginners #17 - Foreign Keys (part 2) - Finished setting up Dojo as a Foreign Key + added Dojo to Ninjas profile page.

--- app/Models/Ninja.php
+++ app/Models/Ninja.php
@@ -11,6 +11,12 @@ class Ninja extends Model
 
     /** @use HasFactory<\Database\Factories\NinjaFactory> */
     use HasFactory;
+
+    public function dojo() {
+        return $this->belongsTo(Dojo::class);
+    }
+
+    //  $ninja->dojo->name
 }
 
 
--- resources/views/Ninjas/show.blade.php
+++ resources/views/Ninjas/show.blade.php
@@ -1,9 +1,18 @@
 <x-layout>
     <h2>{{ $ninja->name }}</h2>
 
-    <div class="bg-gray-200 p-4 rounded">
+    <div class="bg-gray-50 p-4 rounded">
         <p><strong>Skill Level:</strong> {{ $ninja->skill }}</p>
         <p><strong>About me:</strong></p>
         <p><strong>{{ $ninja->bio }}</strong></p>
+    </div>
+
+    {{-- dojo info --}}
+    <div class="border-2 border-dashed bg-white px-4 pb-4 my-4 rounded">
+        <h3></h3>
+        <p><strong>Dojo name:</strong> {{ $ninja->dojo->name }}</p>
+        <p><strong>Location:</strong> {{ $ninja->dojo->location}}</p>
+        <p><strong>About the Dojo:</strong></p>
+        <p>{{ $ninja->dojo->description }}</p>
     </div>
 </x-layout>
