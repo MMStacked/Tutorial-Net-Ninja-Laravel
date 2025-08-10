27e99556c8060fd13a879fc27eaa442f23688938
Awais I <mmstacked@gmail.com>
2025-08-10 18:47:32 +0100

    Completed: Laravel Tutorial for Beginners #20 - Showing Form Errors - Updated error handling to display errors and to also retain info when you have a failed submition

--- resources/views/Ninjas/create.blade.php
+++ resources/views/Ninjas/create.blade.php
@@ -12,6 +12,7 @@
         type="text"
         id="name"
         name="name"
+        value="{{ old('name') }}"
         Required
     >
 
@@ -21,6 +22,7 @@
         type="number"
         id="skill"
         name="skill"
+        value="{{ old('skill') }}"
         required
     >
 
@@ -31,14 +33,14 @@
         id="bio"
         name="bio"
         required
-    ></textarea>
+    >{{ old('bio') }}</textarea>
 
     {{-- Select a Dojo --}}
     <label for="dojo_id">Dojo:</label>
     <select id="dojo_id" name="dojo_id" required>
         <option value="" disabled selected>Select a dojo</option>
         @foreach($dojos as $dojo)
-        <option value="{{ $dojo->id }}">
+        <option value="{{ $dojo->id }}" {{ $dojo->id == old('dojo_id') ? 'selected' : ''}}>
             {{ $dojo->name }}
         </option>
         @endforeach
@@ -47,6 +49,16 @@
     <button type="submit" class="btn mt-4">Create Ninja</button>
 
     {{-- Validation errors --}}
+    @if ($errors->any())
+        <ul class="px-4 py-2 bg-red-100">
+            <p class="my-2 text-red-500">Error:</p>
+            @foreach($errors->all() as $error)
+                <li class="my-2 text-red-500">
+                    - {{ $error }}
+                </li>
+            @endforeach
+        </ul>
+    @endif
 
 </form>
 </x-layout>
